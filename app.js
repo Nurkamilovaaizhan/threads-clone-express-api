5c809659bfc879fcf48e2eb71a248d1b81c26729
Nurkamilovaaizhan <146005300+Nurkamilovaaizhan@users.noreply.github.com>
2024-03-17 20:22:46 +0600

    user controller

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const fs = require('fs')
+require('dotenv').config()
 
 const app = express();
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer')
+const multer = require('multer');
+const { UserController } = require('../controllers');
+const { authenticateToken } = require('../middleware/auth');
 
 // Показываем, где хранить файлы
 const uploadDestination = 'uploads'
@@ -14,8 +16,11 @@ const storage = multer.diskStorage({
 
 const uploads = multer({ storage: storage})
 
-router.get('/register', (req, res) => {
-    res.send('register');
-})
+router.post('/register', UserController.register)
+router.post('/login', UserController.login)
+router.get('/current', authenticateToken, UserController.current)
+router.get('/users/:id', authenticateToken, UserController.getUserById)
+router.put('/users/:id', authenticateToken, UserController.updateUser)
+
 
 module.exports = router;
