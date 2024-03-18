9bc6f4d8566ad410a0221e4921f5ec9cc4cb4563
Nurkamilovaaizhan <146005300+Nurkamilovaaizhan@users.noreply.github.com>
2024-03-19 00:28:49 +0600

    follow controller

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const { UserController, PostController, CommentController, LikeController } = require('../controllers');
+const { UserController, PostController, CommentController, LikeController, FollowController } = require('../controllers');
 const { authenticateToken } = require('../middleware/auth');
 
 // Показываем, где хранить файлы
@@ -36,5 +36,10 @@ router.delete("/comments/:id", authenticateToken, CommentController.deleteCommen
 // роуты лайков
 router.post("/likes", authenticateToken, LikeController.likePost);
 router.delete("/likes/:id", authenticateToken, LikeController.unlikePost);
+
+// роуты подписок
+router.post("/follow", authenticateToken, FollowController.followUser);
+router.delete("/unfollow/:id", authenticateToken, FollowController.unfollowUser);
+
 
 module.exports = router;
